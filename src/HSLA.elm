bb3270134f486a4e33a5c02bb613922d5c9e4309
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-25 17:39:33 +0530

    playing with svg & UI config

--- src/HSLA.elm
+++ src/HSLA.elm
@@ -1,23 +1,21 @@
 module HSLA exposing (HSLA, fromPartial, hsla, toHexAString)
 
 import Color
-import Hex
 import InternalColor
 import RGBA
-import Round
+
+
+type alias HSLA =
+    InternalColor.HSLA
+
+
+type alias PartialHSLA a =
+    { a | hue : Float, saturation : Float, lightness : Float, alpha : Float }
 
 
 hsla : Float -> Float -> Float -> Float -> HSLA
 hsla =
     InternalColor.HSLA
-
-
-type alias HSLA =
-    hsla
-
-
-type alias PartialHSLA a =
-    { a | hue : Float, saturation : Float, lightness : Float, alpha : Float }
 
 
 fromPartial : PartialHSLA a -> HSLA
