30edc4e05f85263d9092cec0e6a8b12372f845b0
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-26 13:32:51 +0530

    playing with svg & UI config

--- src/SvgView.elm
+++ src/SvgView.elm
@@ -5,41 +5,14 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
 
-roundedRect : Html msg
-roundedRect =
-    Svg.rect
-        [ x "100"
-        , y "10"
-        , width "100"
-        , height "100"
-        , rx "15"
-        , ry "15"
-        , color "red"
-        , fill "#caf3f5"
-        ]
-        []
-
-
-svgView : Html msg
-svgView =
-    Svg.svg
-        [ width "100%"
-        , viewBox "0 0 500 100"
-        ]
-        [ Svg.rect [ width "100%", height "100%", fill "#361110" ] []
-        , roundedRect
-        ]
-
-
 view : Html msg
 view =
     svg
-        [ width "120"
-        , height "120"
-        , width "100%"
-        , viewBox "0 0 120 120"
+        [ width "100%"
+        , viewBox "0 0 500 120"
         ]
-        [ rect
+        [ Svg.rect [ width "100%", height "100%", fill "#fff" ] []
+        , rect
             [ x "10"
             , y "10"
             , width "100"
@@ -54,5 +27,15 @@ view =
             , r "50"
             ]
             []
-        , svgView
+        , rect
+            [ x "150"
+            , y "10"
+            , width "100"
+            , height "100"
+            , rx "15"
+            , ry "15"
+            , color "red"
+            , fill "tomato"
+            ]
+            []
         ]
