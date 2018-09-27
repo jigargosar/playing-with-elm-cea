cd5cde107bd0a182a49623020db6a6ccceff1f63
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-27 18:00:36 +0530

    bb2

--- experiments/bouncing-ball2/src/Main.elm
+++ experiments/bouncing-ball2/src/Main.elm
@@ -1,19 +1,31 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Browser.Events
+import Html as H exposing (Html)
+import Html.Attributes as HA
+import Html.Events as HE
+import Round
+import Set
+import String exposing (String)
+import Svg
+import Svg.Attributes as SA
+
 
 
 ---- MODEL ----
+
+
+type alias Flags =
+    {}
 
 
 type alias Model =
     {}
 
 
-init : ( Model, Cmd Msg )
-init =
+init : Flags -> ( Model, Cmd Msg )
+init _ =
     ( {}, Cmd.none )
 
 
@@ -23,22 +35,65 @@ init =
 
 type Msg
     = NoOp
+    | AFrame Float
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+update msg m =
+    case msg of
+        NoOp ->
+            ( m, Cmd.none )
+
+        AFrame delta ->
+            ( m
+            , Cmd.none
+            )
 
 
 
 ---- VIEW ----
 
 
+svgView _ =
+    let
+        w =
+            500
+
+        h =
+            round (w * 2 / 3)
+
+        ballRadius =
+            10
+    in
+    Svg.svg [ HA.width w, HA.height h ]
+        [ Svg.rect [ SA.width "100%", SA.height "100%", SA.fill "#adbeeb" ] []
+        , Svg.circle
+            [ SA.cx "100"
+            , SA.cy "100"
+            , SA.r (ballRadius |> String.fromInt)
+            , SA.fill "#cd37a9"
+            ]
+            []
+        ]
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    H.div []
+        [ H.div [ HA.class "pa3 vs3" ]
+            [ H.div [ HA.class "f1" ] [ H.text "Svg Animation" ]
+            , H.div [ HA.class "" ] [ svgView model ]
+            ]
+        ]
+
+
+
+---- Subscriptions ----
+
+
+subscriptions model =
+    Sub.batch
+        [ Browser.Events.onAnimationFrameDelta AFrame
         ]
 
 
@@ -46,11 +101,11 @@ view model =
 ---- PROGRAM ----
 
 
-main : Program () Model Msg
+main : Program Flags Model Msg
 main =
     Browser.element
         { view = view
-        , init = \_ -> init
+        , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
