8eec0acae5996c6e95bde9179ac8ab46a25ddbdd
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-01 10:43:01 +0530

    a-maze: init

--- experiments/a-maze/src/ViewAMaze.elm
+++ experiments/a-maze/src/ViewAMaze.elm
@@ -53,63 +53,13 @@ mazeCellSizeInPx =
 
 viewMaze maze =
     let
-        drawMazeCellAt cellX cellY =
-            let
-                drawInnerGridCell x y =
-                    let
-                        drawWithFill fillS =
-                            rect
-                                [ (x * mazeInnerCellSizeInPx)
-                                    |> String.fromInt
-                                    |> SA.x
-                                , (y * mazeInnerCellSizeInPx)
-                                    |> String.fromInt
-                                    |> SA.y
-                                , SA.width (mazeInnerCellSizeInPx |> String.fromInt)
-                                , SA.height (mazeInnerCellSizeInPx |> String.fromInt)
-                                , SA.fill fillS
-                                , SA.strokeWidth "0"
-                                , SA.stroke "#000"
-                                ]
-                                []
-
-                        drawPath =
-                            drawWithFill "#cd37a9"
-
-                        drawWall =
-                            drawWithFill "#000"
-                    in
-                    case maze |> AMaze.dataAt x y of
-                        Nothing ->
-                            drawWall
-
-                        Just { down, right } ->
-                            let
-                                isSouthWallCord =
-                                    y >= pathSize
-
-                                isRightWallCord =
-                                    x >= pathSize
-
-                                shouldDrawWall =
-                                    (isSouthWallCord && down)
-                                        || (isRightWallCord && right)
-                            in
-                            ter (isSouthWallCord || isRightWallCord) drawWall drawPath
-            in
-            g
-                [ iTranslate
-                    (cellX * mazeCellSizeInPx)
-                    (cellY * mazeCellSizeInPx)
-                    |> SA.transform
-                ]
-                (mapCoordinates2D
-                    mazeCellSize
-                    mazeCellSize
-                    drawInnerGridCell
-                )
+        dataAt x y =
+            maze |> AMaze.dataAt x y
     in
-    mapCoordinates2D maze.width maze.height drawMazeCellAt
+    mapCoordinates2D
+        maze.width
+        maze.height
+        (drawMazeCellAt dataAt)
         |> g []
 
 
@@ -121,3 +71,60 @@ iTranslate x y =
     , ")"
     ]
         |> String.join ""
+
+
+drawMazeCellAt dataAt cellX cellY =
+    let
+        drawInnerGridCell x y =
+            let
+                drawWithFill fillS =
+                    rect
+                        [ (x * mazeInnerCellSizeInPx)
+                            |> String.fromInt
+                            |> SA.x
+                        , (y * mazeInnerCellSizeInPx)
+                            |> String.fromInt
+                            |> SA.y
+                        , SA.width (mazeInnerCellSizeInPx |> String.fromInt)
+                        , SA.height (mazeInnerCellSizeInPx |> String.fromInt)
+                        , SA.fill fillS
+                        , SA.strokeWidth "0"
+                        , SA.stroke "#000"
+                        ]
+                        []
+
+                drawPath =
+                    drawWithFill "#cd37a9"
+
+                drawWall =
+                    drawWithFill "#000"
+            in
+            case dataAt x y of
+                Nothing ->
+                    drawWall
+
+                Just { down, right } ->
+                    let
+                        isSouthWallCord =
+                            y >= pathSize
+
+                        isRightWallCord =
+                            x >= pathSize
+
+                        shouldDrawWall =
+                            (isSouthWallCord && down)
+                                || (isRightWallCord && right)
+                    in
+                    ter (isSouthWallCord || isRightWallCord) drawWall drawPath
+    in
+    g
+        [ iTranslate
+            (cellX * mazeCellSizeInPx)
+            (cellY * mazeCellSizeInPx)
+            |> SA.transform
+        ]
+        (mapCoordinates2D
+            mazeCellSize
+            mazeCellSize
+            drawInnerGridCell
+        )
