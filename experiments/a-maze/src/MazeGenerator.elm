0c9b1eddf57d3c7485687e3d5fc2ce1455dd4ae9
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-01 19:15:57 +0530

    a-maze: init

--- experiments/a-maze/src/MazeGenerator.elm
+++ experiments/a-maze/src/MazeGenerator.elm
@@ -47,3 +47,50 @@ type MazeGenerator
 init : Int -> Int -> MazeGenerator
 init width height =
     MazeGenerator { defaultRecord | width = width, height = height }
+
+
+step : MazeGenerator -> MazeGenerator
+step (MazeGenerator rec) =
+    (if rec.visited.size == rec.width * rec.height then
+        rec
+
+     else
+        case rec.stack of
+            c :: rest ->
+                stepHelp c rec
+
+            _ ->
+                rec
+    )
+        |> MazeGenerator
+
+
+stepHelp : Record -> Record
+stepHelp stackTop rec =
+    let
+        totalCellCount =
+            rec.width * rec.height
+
+        _ =
+            ( Set.size rec.visited, totalCellCount ) |> Debug.log "(visited,total)"
+
+        neighbourCordGenerator =
+            getUnVisitedNeighboursOfTopOfStack m
+                |> Debug.log "getValidNeighbourCords"
+                |> Random.List.choose
+
+        ( ( maybeCord, _ ), newSeed ) =
+            Random.step neighbourCordGenerator m.seed
+
+        _ =
+            maybeCord |> Debug.log "randomCord"
+
+        newModel =
+            { m | seed = newSeed }
+    in
+    case maybeCord of
+        Just cord ->
+            updateVisitCell cord newModel
+
+        Nothing ->
+            updatePopStack newModel
