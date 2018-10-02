18d9f1d9eee9848a1b676d376ebb5ca8e4b26f76
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-02 18:48:04 +0530

    svg-api: init

--- experiments/svg-api/src/Main.elm
+++ experiments/svg-api/src/Main.elm
@@ -62,7 +62,11 @@ view : Model -> Html Msg
 view model =
     div [ class "measure-wide center pa2 h-100 flex flex-column vs3 " ]
         [ div [ class "f3" ] [ text "SVG API" ]
-        , Svg.svg [ SA.width "100%", SA.height "100%", SA.fill "transparent" ]
+        , Svg.svg
+            [ SA.width "100%"
+            , SA.height "100%"
+            , SA.fill "transparent"
+            ]
             [ Svg.rect
                 [ SA.width "100%"
                 , SA.height "100%"
