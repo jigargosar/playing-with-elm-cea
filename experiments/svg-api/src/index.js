eabcfd2b8418b64b9ece58433b9f78467b74ec0a
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-02 18:34:40 +0530

    svg-api: init

--- experiments/svg-api/src/index.js
+++ experiments/svg-api/src/index.js
@@ -2,11 +2,14 @@ import './main.css'
 import { Elm } from './Main.elm'
 import registerServiceWorker from './registerServiceWorker'
 
+let documentElement = document.documentElement
 Elm.Main.init({
   node: document.getElementById('root'),
   flags: {
-    now:Date.now()
-  }
-});
+    now: Date.now(),
+    vw: Math.max(documentElement.clientWidth, window.innerWidth || 0),
+    vh: Math.max(documentElement.clientHeight, window.innerHeight || 0),
+  },
+})
 
-registerServiceWorker();
+registerServiceWorker()
