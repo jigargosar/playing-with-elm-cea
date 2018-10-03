90d119b1822bbd9d744ef12e5b6ec210579577c9
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-03 16:21:07 +0530

    svg-api: init

--- experiments/svg-api/src/Main.elm
+++ experiments/svg-api/src/Main.elm
@@ -74,10 +74,10 @@ isDownDown =
 getArrowKeyXYDirection m =
     let
         xDirection =
-            ter (isLeftDown m) (-1) (ter (isRightDown m) (1) (0))
+            ter (isLeftDown m) -1 0 + ter (isRightDown m) 1 0
 
         yDirection =
-            ter (isUpDown m) (-1) (ter (isDownDown m) (1) (0))
+            ter (isUpDown m) -1 0 + (ter (isDownDown m) 1 0)
     in
         ( xDirection, yDirection )
 
