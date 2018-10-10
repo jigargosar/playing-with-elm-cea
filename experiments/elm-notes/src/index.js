eadb51e19240cc43ce8f31b81ac164e8c13c7630
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-10 21:28:14 +0530

    Edit Note

--- experiments/elm-notes/src/index.js
+++ experiments/elm-notes/src/index.js
@@ -2,11 +2,13 @@ import 'tachyons'
 import './main.css'
 import { Elm } from './Main.elm'
 import registerServiceWorker from './registerServiceWorker'
+import { isNil } from 'ramda'
 
 const app = Elm.Main.init({
   node: document.getElementById('root'),
   flags: {
     now: Date.now(),
+    noteCollection: storageGetOr({}, 'noteCollection'),
   },
 })
 
@@ -19,4 +21,20 @@ subscribe(
 
 function subscribe(port, fn, app) {
   app.ports[port].subscribe(fn)
+}
+
+function storageGetOr(or, key) {
+  try {
+    return JSON.parse(localStorage.getItem(key))
+  } catch (e) {
+    return or
+  }
+}
+
+function storageSet(key, value) {
+  if (isNil(value) || isNil(key)) {
+    console.warn('Invalid Args', 'storageSet', key, value)
+    return
+  }
+  localStorage.setItem(key, JSON.stringify(value))
 }
