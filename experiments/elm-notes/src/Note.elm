a7192ef195807f7cc2e7849a97e7d1d5823fb290
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-11 17:09:58 +0530

    elm-notes: firebase firestore

--- experiments/elm-notes/src/Note.elm
+++ experiments/elm-notes/src/Note.elm
@@ -8,8 +8,52 @@ import Json.Decode.Pipeline as D
 import Json.Encode as E
 
 
+encode note =
+    E.object
+        [ ( "id", Id.encode note.id )
+        , ( "content", E.string note.content )
+        , ( "deleted", E.bool note.deleted )
+        , ( "createdAt", E.int note.createdAt )
+        , ( "modifiedAt", E.int note.modifiedAt )
+        ]
+
+
+tsDecoder now =
+    D.int
+        |> D.map
+            (\ts ->
+                if ts == 0 then
+                    now
+                else
+                    ts
+            )
+
+
+defaultTo val failDecoder =
+    D.oneOf [ failDecoder, D.succeed val ]
+
+
+optionalOr propName propDecoder defaultValue =
+    D.oneOf [ D.field propName propDecoder, D.succeed defaultValue ]
+
+
+decoder : Decoder Note
+decoder =
+    D.map5 Note
+        (D.field "content" D.string)
+        (optionalOr "deleted" D.bool False)
+        (D.field "createdAt" D.int)
+        (D.field "modifiedAt" D.int)
+        (D.field "id" Id.decoder)
+
+
 type alias Note =
-    { content : String, createdAt : Int, modifiedAt : Int, id : Id }
+    { content : String, deleted : Bool, createdAt : Int, modifiedAt : Int, id : Id }
+
+
+generator : Int -> String -> Random.Generator Note
+generator now content =
+    Random.map (Note content False now now) IdX.generator
 
 
 title =
@@ -27,39 +71,5 @@ updateContent now content note =
         { note | content = content, modifiedAt = now }
 
 
-generator : Int -> String -> Random.Generator Note
-generator now content =
-    Random.map (Note content now now) IdX.generator
-
-
 idStr =
     .id >> Id.toString
-
-
-encode note =
-    E.object
-        [ ( "id", Id.encode note.id )
-        , ( "content", E.string note.content )
-        , ( "createdAt", E.int note.createdAt )
-        , ( "modifiedAt", E.int note.modifiedAt )
-        ]
-
-
-tsDecoder now =
-    D.int
-        |> D.map
-            (\ts ->
-                if ts == 0 then
-                    now
-                else
-                    ts
-            )
-
-
-decoder : Decoder Note
-decoder =
-    D.map4 Note
-        (D.field "content" D.string)
-        (D.field "createdAt" D.int)
-        (D.field "modifiedAt" D.int)
-        (D.field "id" Id.decoder)
