3fecf4eb6bd306803aa8fe330cab56bef3a25fea
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-16 17:24:33 +0530

    elm-notes: Pages.EditNote Throttle/EditableInput

--- experiments/elm-notes/src/Pages/EditNote.elm
+++ experiments/elm-notes/src/Pages/EditNote.elm
@@ -38,14 +38,6 @@ type Reply
     = SaveContent Note.Note NoteContent
 
 
-andThen f ( m1, c1 ) =
-    let
-        ( m2, c2 ) =
-            f m1
-    in
-        ( m2, Cmd.batch [ c1, c2 ] )
-
-
 maybeBool bool value =
     if bool then
         Just value
