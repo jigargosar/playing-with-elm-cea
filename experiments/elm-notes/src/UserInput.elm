fda8ea4c58ed241304041689b02b6b7deeeacb70
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-16 16:43:02 +0530

    elm-notes: Pages.EditNote Throttle/EditableInput

--- experiments/elm-notes/src/UserInput.elm
+++ experiments/elm-notes/src/UserInput.elm
@@ -8,3 +8,10 @@ type alias Model a =
     { editable : EditableInput.Model a
     , throttleSave : Throttle.Model
     }
+
+
+init : a -> Model a
+init value =
+    { editable = EditableInput.init value
+    , throttleSave = Throttle.init 3000
+    }
