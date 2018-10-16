b36c5b8c0671bed8d556b793999d73260f8a6b59
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-16 16:56:19 +0530

    elm-notes: Pages.EditNote Throttle/EditableInput

--- experiments/elm-notes/src/UserInput.elm
+++ experiments/elm-notes/src/UserInput.elm
@@ -15,3 +15,17 @@ init value =
     { editable = EditableInput.init value
     , throttleSave = Throttle.init 3000
     }
+
+
+onChange : msg -> a -> Model a -> ( Model a, Cmd msg )
+onChange throttledSaveMsg newValue model =
+    let
+        ( newThrottleSave, cmd ) =
+            Throttle.push throttledSaveMsg model.throttleSave
+    in
+        ( { model
+            | editable = model.editable |> EditableInput.set newValue
+            , throttleSave = newThrottleSave
+          }
+        , cmd
+        )
