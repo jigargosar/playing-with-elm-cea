3c9968ed95311d9fffad8bcf639d8c34367a6c20
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-16 16:25:41 +0530

    elm-notes: Pages.EditNote Throttle/EditableInput

--- experiments/elm-notes/src/Throttle.elm
+++ experiments/elm-notes/src/Throttle.elm
@@ -28,34 +28,3 @@ push msg model =
 
 updateOnEmit model =
     { model | scheduled = False }
-
-
-type Msg
-    = Emit
-    | EventOccurred
-
-
-type Reply
-    = Emitted
-
-
-update msg model =
-    let
-        _ =
-            Debug.log "Trigger: Pre Update" ( msg, model )
-    in
-        (case msg of
-            Emit ->
-                ( { model | scheduled = False }, Cmd.none, Just Emitted )
-
-            EventOccurred ->
-                if model.scheduled then
-                    ( model, Cmd.none, Nothing )
-                else
-                    ( { model | scheduled = True }
-                    , Process.sleep model.ms
-                        |> Task.perform (always Emit)
-                    , Nothing
-                    )
-        )
-            |> Debug.log "Trigger: Post Update"
