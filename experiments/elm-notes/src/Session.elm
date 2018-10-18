05ef7ecffbc888f07a6442ad4a91a8a20fc246c5
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-18 10:15:09 +0530

    elm-notes: Step Url Model for SPA

--- experiments/elm-notes/src/Session.elm
+++ experiments/elm-notes/src/Session.elm
@@ -1,9 +1,39 @@
 module Session exposing (..)
 
+import Json.Decode exposing (Decoder)
+import Json.Decode as D
+import Json.Encode as E
+
 
 type alias Session =
-    {}
+    { authState : AuthState }
 
 
 empty =
-    {}
+    { authState = InitialUnknown }
+
+
+type alias UserDetails =
+    { uid : String
+    , email : String
+    , displayName : String
+    }
+
+
+type AuthState
+    = Authenticated UserDetails
+    | Anon
+    | InitialUnknown
+
+
+userDetailsDecoder : Decoder UserDetails
+userDetailsDecoder =
+    D.map3 UserDetails
+        (D.field "uid" D.string)
+        (D.field "email" D.string)
+        (D.field "displayName" D.string)
+
+
+authStateDecoder : Decoder AuthState
+authStateDecoder =
+    D.oneOf [ D.null Anon, D.map Authenticated userDetailsDecoder ]
