ee0ad2fe4404d45579f48437b949e918d1fc2e44
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-20 17:41:20 +0530

    elm-swipe-do: init

--- experiments/elm-swipe-do/src/index.js
+++ experiments/elm-swipe-do/src/index.js
@@ -1,9 +1,14 @@
+import 'tachyons'
 import './main.css'
 import { Elm } from './Main.elm'
 import registerServiceWorker from './registerServiceWorker'
 
-Elm.Main.init({
-  node: document.getElementById('root')
-});
+const app = Elm.Main.init({
+  node: document.getElementById('root'),
+  flags: {
+    now: Date.now(),
+    noteList: storageGetOr({}, 'noteCollection'),
+  },
+})
 
-registerServiceWorker();
+registerServiceWorker()
