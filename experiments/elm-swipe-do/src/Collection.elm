b87314acc5ba15fdb1974fab9dc1b4beb0737f72
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-21 23:14:02 +0530

    elm-swipe-do: magic menu

---
+++ experiments/elm-swipe-do/src/Collection.elm
@@ -0,0 +1,129 @@
+module Collection exposing
+    ( Collection
+    , Id
+    , Ids
+    , Millis
+    , Model
+    , createAndAdd
+    , encode
+    , generator
+    , get
+    , getByIdSet
+    , idList
+    , items
+    , replace
+    , updateIdSetWith
+    , updateWith
+    )
+
+import Dict exposing (Dict)
+import IdX
+import Json.Decode as D exposing (Decoder)
+import Json.Encode as E
+import Random
+import Set exposing (Set)
+import Task exposing (Task)
+import Time
+
+
+type alias Id =
+    String
+
+
+type alias Ids =
+    Set Id
+
+
+type alias Millis =
+    Int
+
+
+type alias Model item =
+    { dict : Dict String item, seed : Random.Seed }
+
+
+type alias Collection item =
+    Model item
+
+
+overDict : (Dict String item -> Dict String item) -> Model item -> Model item
+overDict updateFn model =
+    { model | dict = updateFn model.dict }
+
+
+insert : Id -> item -> Model item -> Model item
+insert id item =
+    overDict <| Dict.insert id item
+
+
+update : Id -> (item -> item) -> Model item -> Model item
+update id f =
+    overDict <| Dict.update id (Maybe.map f)
+
+
+createAndAdd : (Id -> Millis -> item) -> Model item -> Task x ( item, Model item )
+createAndAdd initItem model =
+    let
+        ( id, newSeed ) =
+            Random.step IdX.stringIdGenerator model.seed
+    in
+    createAndAddHelp id initItem { model | seed = newSeed }
+
+
+createAndAddHelp : Id -> (Id -> Millis -> item) -> Model item -> Task x ( item, Model item )
+createAndAddHelp id initItemFn model =
+    taskNowMilli
+        |> Task.map (initItemFn id >> (\item -> ( item, insert id item model )))
+
+
+taskNowMilli : Task x Int
+taskNowMilli =
+    Time.now |> Task.map Time.posixToMillis
+
+
+updateWith : Id -> (Millis -> item -> item) -> Model item -> Task x (Model item)
+updateWith id updateFn model =
+    taskNowMilli
+        |> Task.map (\nowMilli -> update id (updateFn nowMilli) model)
+
+
+updateIdSetWith : Set Id -> (Millis -> item -> item) -> Model item -> Task x (Model item)
+updateIdSetWith idSet updateFn model =
+    taskNowMilli
+        |> Task.map (\nowMilli -> idSet |> Set.foldl (\id -> update id (updateFn nowMilli)) model)
+
+
+encode : (item -> E.Value) -> Model item -> E.Value
+encode encodeItem model =
+    model.dict |> E.dict identity encodeItem
+
+
+generator : Decoder item -> E.Value -> Random.Generator (Model item)
+generator itemDecoder encodedDict =
+    Random.map (Model Dict.empty) Random.independentSeed
+        |> Random.map (replace itemDecoder encodedDict)
+
+
+replace : Decoder item -> E.Value -> Model item -> Model item
+replace itemDecoder encodedDict model =
+    D.decodeValue (D.dict itemDecoder) encodedDict
+        |> Result.map (\dict -> overDict (always dict) model)
+        |> Result.mapError identity
+        |> Result.withDefault model
+
+
+items =
+    .dict >> Dict.values
+
+
+idList =
+    .dict >> Dict.keys
+
+
+get id =
+    .dict >> Dict.get id
+
+
+getByIdSet : Set Id -> Collection item -> List item
+getByIdSet idSet =
+    .dict >> Dict.filter (\id _ -> idSet |> Set.member id) >> Dict.values
--- experiments/elm-swipe-do/src/Todo.elm
+++ experiments/elm-swipe-do/src/Todo.elm
@@ -1,11 +1,14 @@
 module Todo exposing (Content, Id, Todo, generator, init)
 
+import Collection exposing (Millis)
 import IdX
+import Json.Decode as D exposing (Decoder)
+import Json.Encode as E
 import Random
 
 
 type alias Id =
-    String
+    Collection.Id
 
 
 type alias Content =
@@ -17,15 +20,58 @@ type alias Todo =
 
 
 type alias Model =
-    { id : Id
-    , content : Content
+    { content : Content
     , done : Bool
+    , deleted : Bool
+    , createdAt : Int
+    , modifiedAt : Int
+    , id : Collection.Id
     }
 
 
-init id =
-    { id = id, content = "", done = False }
+encode model =
+    E.object
+        [ ( "id", E.string model.id )
+        , ( "content", E.string model.content )
+        , ( "done", E.bool model.deleted )
+        , ( "deleted", E.bool model.deleted )
+        , ( "createdAt", E.int model.createdAt )
+        , ( "modifiedAt", E.int model.modifiedAt )
+        ]
 
 
-generator =
-    Random.map init IdX.stringIdGenerator
+decoder : Decoder Model
+decoder =
+    D.map6 Model
+        (D.field "content" D.string)
+        (D.field "done" D.bool)
+        (D.field "deleted" D.bool)
+        (D.field "createdAt" D.int)
+        (D.field "modifiedAt" D.int)
+        (D.field "id" D.string)
+
+
+init =
+    initWithContent ""
+
+
+initWithContent content id now =
+    { content = content, done = False, deleted = False, id = id, createdAt = now, modifiedAt = now }
+
+
+updateContent : Content -> Collection.Millis -> Model -> Model
+updateContent content now model =
+    if content == model.content then
+        model
+
+    else
+        { model | content = content, modifiedAt = now }
+
+
+delete : Collection.Millis -> Model -> Model
+delete now model =
+    if model.deleted then
+        model
+
+    else
+        { model | deleted = True, modifiedAt = now }
