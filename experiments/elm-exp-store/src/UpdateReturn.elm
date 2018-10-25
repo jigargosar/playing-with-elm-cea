aa483dbdf96b4b37dde4746044f446b0b04bee4c
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-25 21:41:06 +0530

    elm-exp-store: store with update fn

--- experiments/elm-exp-store/src/UpdateReturn.elm
+++ experiments/elm-exp-store/src/UpdateReturn.elm
@@ -1,4 +1,4 @@
-module UpdateReturn exposing (addOutMsg, andThen, pure)
+module UpdateReturn exposing (addOutMsg, andThen, andThen3, pure)
 
 import Update3
 
@@ -17,3 +17,11 @@ andThen f ( m1, c1 ) =
 
 addOutMsg =
     Update3.addOutMsg
+
+
+andThen3 f ( m1, c1, o1 ) =
+    let
+        ( m2, c2, o2 ) =
+            f o1 m1
+    in
+    ( m2, Cmd.batch [ c1, c2 ], o2 )
