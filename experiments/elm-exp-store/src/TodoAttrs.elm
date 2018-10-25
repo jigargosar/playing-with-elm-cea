a6333e323a9d5c0fa7aceb32727cfc557524a1b2
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-25 13:57:55 +0530

    elm-exp-store: store with update fn

--- experiments/elm-exp-store/src/TodoAttrs.elm
+++ experiments/elm-exp-store/src/TodoAttrs.elm
@@ -1,5 +1,6 @@
 module TodoAttrs exposing (Content, TodoAttrs, decoder, init, initEmpty, setContent)
 
+import BasicsX exposing (maybeBool)
 import Json.Decode as D exposing (Decoder)
 import Json.Encode as E
 import Store exposing (Item)
@@ -34,3 +35,18 @@ decoder =
 setContent : Content -> TodoAttrs -> TodoAttrs
 setContent content model =
     { model | content = content }
+
+
+type Msg
+    = NoOp
+    | SetContent Content
+
+
+update : Msg -> TodoAttrs -> Maybe TodoAttrs
+update message model =
+    case message of
+        NoOp ->
+            Nothing
+
+        SetContent newContent ->
+            maybeBool (model.content /= newContent) { model | content = newContent }
