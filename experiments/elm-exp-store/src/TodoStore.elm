4a4632457547a66a944db91ee931721daf8dd6e8
Jigar Gosar <jigar.gosar@gmail.com>
2018-10-31 15:38:11 +0530

    elm-exp-store: store refactoring 2

--- experiments/elm-exp-store/src/TodoStore.elm
+++ experiments/elm-exp-store/src/TodoStore.elm
@@ -22,3 +22,16 @@ type alias Todo =
     , completed : Bool
     , scheduledAt : Int
     }
+
+
+todoCodec : Codec Todo
+todoCodec =
+    Todo
+        |> JC.first "id" JC.string .id
+        |> JC.next "createdAt" JC.int .createdAt
+        |> JC.next "modifiedAt" JC.int .modifiedAt
+        |> JC.next "deleted" JC.bool .deleted
+        |> JC.next "content" JC.string .content
+        |> JC.option "completed" JC.bool .completed False
+        |> JC.option "scheduledAt" JC.int .scheduledAt 0
+        |> JC.end
