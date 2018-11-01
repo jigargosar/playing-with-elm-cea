e139bbb9a11e3e80b38ae2a77481f30694fe5b51
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-01 14:59:54 +0530

    init: elm-install-interactive

--- experiments/elm-ever-share/ei.js
+++ experiments/elm-ever-share/ei.js
@@ -3,8 +3,8 @@ const ora = require('ora')
 const got = require('got')
 const TeenProcess = require('teen_process')
 
-async function tailFileForABit() {
-  let teen = new TeenProcess.SubProcess('elm', ['install', 'elm/svg'])
+async function elmInstall(packageName) {
+  let teen = new TeenProcess.SubProcess('elm', ['install', packageName])
   teen.on('stream-line', console.log)
   // console.log('teen',teen)
   await teen.start(0)
@@ -19,47 +19,50 @@ async function tailFileForABit() {
   // console.log('teen',teen)
 }
 
-tailFileForABit().catch(console.error)
+// elmInstall().catch(console.error)
 
-// inquirer.registerPrompt('search-checkbox', require('inquirer-search-checkbox'))
-//
-// async function fetchElmSearchJSON() {
-//   const spinner = ora({ text: 'Fetching Elm Package Index' }).start()
-//   try {
-//     const response = await got('package.elm-lang.org/search.json', {
-//       json: true,
-//     })
-//     return response.body
-//   } catch (error) {
-//     console.log(error.response.body)
-//     throw error
-//   } finally {
-//     spinner.stop()
-//   }
-// }
-//
-// const boot = async () => {
-//   const searchJSON = await fetchElmSearchJSON()
-//   const answers = await inquirer.prompt([
-//     {
-//       type: 'search-checkbox',
-//       message: 'Select Elm Packages to Install',
-//       name: 'elmPackages',
-//       choices: searchJSON,
-//       validate: function (answer) {
-//         if (answer.length < 1) {
-//           return 'You must choose at least one topping.'
-//         }
-//         return true
-//       },
-//     },
-//     {
-//       type: 'confirm',
-//       message: 'Proceed with install',
-//       name: 'installConfirmed',
-//     },
-//   ])
-//   console.log('answers', answers)
-// }
-//
-// boot().catch(console.error)
+inquirer.registerPrompt('search-checkbox', require('inquirer-search-checkbox'))
+
+async function fetchElmSearchJSON() {
+  const spinner = ora({ text: 'Fetching Elm Package Index' }).start()
+  try {
+    const response = await got('package.elm-lang.org/search.json', {
+      json: true,
+    })
+    return response.body
+  } catch (error) {
+    console.log(error.response.body)
+    throw error
+  } finally {
+    spinner.stop()
+  }
+}
+
+const boot = async () => {
+  const searchJSON = await fetchElmSearchJSON()
+  const answers = await inquirer.prompt([
+    {
+      type: 'search-checkbox',
+      message: 'Select Elm Packages to Install',
+      name: 'elmPackages',
+      choices: searchJSON,
+      validate: function(answer) {
+        if (answer.length < 1) {
+          return 'You must choose at least one topping.'
+        }
+        return true
+      },
+    },
+    {
+      type: 'confirm',
+      message: 'Proceed with install',
+      name: 'installConfirmed',
+    },
+  ])
+  console.log('answers', answers)
+  if (answers.installConfirmed) {
+    await Promise.all(answers.elmPackages.map(elmInstall))
+  }
+}
+
+boot().catch(console.error)
