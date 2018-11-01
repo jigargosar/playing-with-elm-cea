6dc902bf9b1c9db0bcd57aa130f687a91b02160d
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-01 17:45:20 +0530

    init: elm-install-interactive

--- experiments/elm-ever-share/em.js
+++ experiments/elm-ever-share/em.js
@@ -50,9 +50,7 @@ async function fetchPackageElmJson(packageInfo) {
 function findPackageWithExposedModule(moduleName, elmJSONResults) {
   return R.compose(
     R.map(R.pick(['name', 'exposed-modules'])),
-    R.filter(ejs => {
-      ejs['exposed-modules'].includes(moduleName)
-    }),
+    R.filter(ejs => ejs['exposed-modules'].includes(moduleName)),
   )(elmJSONResults)
 }
 
