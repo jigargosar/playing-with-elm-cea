3f23c2a797e2e96821bdb52e5de8afc9ac3447b3
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-01 14:14:47 +0530

    init: elm-install-interactive

--- experiments/elm-ever-share/ei.js
+++ experiments/elm-ever-share/ei.js
@@ -1,51 +1,41 @@
 const inquirer = require('inquirer')
 const ora = require('ora')
+const got = require('got')
 
 inquirer.registerPrompt('search-checkbox', require('inquirer-search-checkbox'))
 
-const spinner = ora({ text: 'Loading unicorns' }).start()
-
-setTimeout(() => {
-  // spinner.color = 'yellow';
-  // spinner.text = 'Loading rainbows';
-  spinner.stop()
-  startInquirer().catch(console.error)
-}, 1000)
-
-async function startInquirer() {
-  return inquirer
-    .prompt([
-      {
-        type: 'search-checkbox',
-        message: 'Select toppings',
-        name: 'toppings',
-        choices: [
-          {
-            name: 'Pepperoni',
-          },
-          {
-            name: 'Ham',
-          },
-          {
-            name: 'Ground Meat',
-          },
-          {
-            name: 'Bacon',
-          },
-          {
-            name: 'Mozzarella',
-          },
-        ],
-        validate: function(answer) {
-          if (answer.length < 1) {
-            return 'You must choose at least one topping.'
-          }
-          return true
-        },
-      },
-    ])
-    .then(function(answers) {
-      console.log(JSON.stringify(answers, null, '  '))
+async function fetchElmSearchJSON() {
+  const spinner = ora({ text: 'Fetching Elm Package Index' }).start()
+  try {
+    const response = await got('package.elm-lang.org/search.json', {
+      json: true,
     })
-    .catch(e => console.log(e))
+    return response.body
+  } catch (error) {
+    console.log(error.response.body)
+    throw error
+  } finally {
+    spinner.stop()
+  }
 }
+
+const boot = async () => {
+  const searchJSON = await fetchElmSearchJSON()
+  const answers = await inquirer.prompt([
+    {
+      type: 'search-checkbox',
+      message: 'Select Elm Packages to Install',
+      name: 'elmPackages',
+      choices: searchJSON,
+      validate: function(answer) {
+        if (answer.length < 1) {
+          return 'You must choose at least one topping.'
+        }
+        return true
+      },
+    },
+  ])
+  console.log('answers', answers)
+}
+
+boot().catch(console.error)
