343480c794ab896a04bd8e44d66961c131e1df12
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-02 14:05:51 +0530

    elm-exp-store: Context Todo List

--- experiments/elm-exp-store/src/Main.elm
+++ experiments/elm-exp-store/src/Main.elm
@@ -471,7 +471,7 @@ viewContextTodoListHeader { contextName, isContextNameClickable, onContextNameCl
                 (contextSelectViewModel
                     |> List.map
                         (\( optionText, optionValue, isSelected ) ->
-                            option [ selected isSelected, value optionValue ] [ txtC "ttu" optionText ]
+                            option [ selected isSelected, value optionValue ] [ optionText ]
                         )
                 )
             ]
