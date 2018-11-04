e85bd09985a08bac02d1fef0203f993c82674cd6
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-04 12:05:15 +0530

    elm-exp-store: Select Context Debouncer

--- experiments/elm-exp-store/src/index.js
+++ experiments/elm-exp-store/src/index.js
@@ -4,13 +4,13 @@ import { Elm } from './Main.elm'
 import registerServiceWorker from './registerServiceWorker'
 import {
   compose,
+  curry,
   forEachObjIndexed,
   ifElse,
   invoker,
   isEmpty,
   isNil,
   partial,
-  partialRight,
   pathOr,
   pick,
   reject,
@@ -20,96 +20,16 @@ import debounce from 'lodash.debounce'
 
 import flyd from 'flyd'
 
-let initialTodos = storageGetOr({}, 'todos')
-let initialContexts = storageGetOr({}, 'contexts')
-// console.log("initialTodos",initialTodos)
-const app = Elm.Main.init({
-  node: document.getElementById('root'),
-  flags: {
-    now: Date.now(),
-    todos: initialTodos,
-    contexts: initialContexts,
-  },
-})
-
-console.log(app.ports)
-
-
-// let documentHasFocus = document.hasFocus
-
-// const winFocus$ = flyd.stream()
-// const winBlur$  = flyd.stream()
-
-
-const winFocusBlur$ = flyd.stream()
-
-winFocusBlur$(document.hasFocus())
-
-window.addEventListener('focus', winFocusBlur$)
-window.addEventListener('blur', winFocusBlur$)
-
-const documentHasFocus$ = flyd.map(compose(()=>document.hasFocus()),winFocusBlur$)
-
-flyd.on(console.warn, documentHasFocus$)
-
-// window.addEventListener('focus', function (e) {
-//   console.log("Out",document.hasFocus(), e)
-//   // recordActiveElement(app)
-// })
-//
-// window.addEventListener('blur', function (e) {
-//   console.log("In",document.hasFocus(), e)
-//
-//   // recordActiveElement(app)
-// })
-
-
-
-// window.document.addEventListener('focusout', function (e) {
-//   console.log("Out",document.hasFocus(), e)
-//   // recordActiveElement(app)
-// })
-//
-// window.document.addEventListener('focusin', function (e) {
-//   console.log("In",document.hasFocus(), e)
-//
-//   // recordActiveElement(app)
-// })
-
-window.addEventListener('wheel', function (e) {
-  // console.log(e)
-  const data = pick(['deltaX', 'deltaY'])(e)
-  sendData(data, 'wheel', app)
-})
-
-
-subscribe(
-  {
-    warn: data => {
-      console.warn(data)
-    },
-    cacheTodoStore: todos => {
-      storageSet('todos', todos)
-    },
-    cacheContextStore: contexts => {
-      storageSet('contexts', contexts)
-    },
-    // focusSelector,
-  },
-  app,
-)
-
-registerServiceWorker()
-
 // Helpers
 
-function sendData(data, port, app) {
-  if (!pathOr(null, ['ports', port, 'send'])(app)) {
-    console.error('sendData port not found', port, 'data ignored', data)
-    return
-  }
-  app.ports[port].send(data)
-}
+const sendTo = curry(function sendTo(app, port, data) {
+    if (!pathOr(null, ['ports', port, 'send'])(app)) {
+      console.error('sendTo port not found', port, 'data ignored', data)
+      return
+    }
+    app.ports[port].send(data)
+  },
+)
 
 function subscribe(options, app) {
   if (!app || !app.ports) {
@@ -123,7 +43,7 @@ function subscribe(options, app) {
       return
     }
     // noinspection JSIgnoredPromiseFromCall, JSCheckFunctionSignatures
-    app.ports[sub].subscribe(data => fn(data, partialRight(sendData, [app])))
+    app.ports[sub].subscribe(data => fn(data, (sendTo([app]))))
   })(options)
 }
 
@@ -159,6 +79,88 @@ export const recordActiveElement = debounce(function recordActiveElement(app) {
     const parentIds = compose(reject(isEmpty), unfold(node => node ? [node.id, node.parentElement] : false),
     )(document.activeElement)
 
-    sendData(parentIds, 'activeElementsParentIdList', app)
+    sendTo(app, 'activeElementsParentIdList', parentIds)
   }
 }, 0, { trailing: true, leading: false })
+
+// App
+
+let initialTodos = storageGetOr({}, 'todos')
+let initialContexts = storageGetOr({}, 'contexts')
+// console.log("initialTodos",initialTodos)
+const app = Elm.Main.init({
+  node: document.getElementById('root'),
+  flags: {
+    now: Date.now(),
+    todos: initialTodos,
+    contexts: initialContexts,
+  },
+})
+
+const sendToApp = partial(sendTo, [app])
+
+console.log(app.ports)
+
+// let documentHasFocus = document.hasFocus
+
+// const winFocus$ = flyd.stream()
+// const winBlur$  = flyd.stream()
+
+const winFocusBlur$ = flyd.stream()
+
+winFocusBlur$(document.hasFocus())
+
+window.addEventListener('focus', winFocusBlur$)
+window.addEventListener('blur', winFocusBlur$)
+
+const documentHasFocus$ = flyd.map(compose(() => document.hasFocus()), winFocusBlur$)
+
+flyd.on(console.warn, documentHasFocus$)
+flyd.on(sendToApp('documentFocusChanged'), documentHasFocus$)
+
+// window.addEventListener('focus', function (e) {
+//   console.log("Out",document.hasFocus(), e)
+//   // recordActiveElement(app)
+// })
+//
+// window.addEventListener('blur', function (e) {
+//   console.log("In",document.hasFocus(), e)
+//
+//   // recordActiveElement(app)
+// })
+
+// window.document.addEventListener('focusout', function (e) {
+//   console.log("Out",document.hasFocus(), e)
+//   // recordActiveElement(app)
+// })
+//
+// window.document.addEventListener('focusin', function (e) {
+//   console.log("In",document.hasFocus(), e)
+//
+//   // recordActiveElement(app)
+// })
+
+window.addEventListener('wheel', function (e) {
+  // console.log(e)
+  const data = pick(['deltaX', 'deltaY'])(e)
+  sendTo(app, 'wheel', data)
+})
+
+subscribe(
+  {
+    warn: data => {
+      console.warn(data)
+    },
+    cacheTodoStore: todos => {
+      storageSet('todos', todos)
+    },
+    cacheContextStore: contexts => {
+      storageSet('contexts', contexts)
+    },
+    // focusSelector,
+  },
+  app,
+)
+
+registerServiceWorker()
+
