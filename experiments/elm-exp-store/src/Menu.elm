4fcacd106a76fa1fd844d86a0acbbf5f96bbb739
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-07 15:17:18 +0530

    elm-exp-store: elm-css

--- experiments/elm-exp-store/src/Menu.elm
+++ experiments/elm-exp-store/src/Menu.elm
@@ -1,5 +1,6 @@
 module Menu exposing (render)
 
+import Css exposing (..)
 import Html.Styled exposing (Html, div, styled)
 import Html.Styled.Keyed exposing (node)
 import Styles exposing (..)
@@ -14,7 +15,7 @@ type alias ViewConfig child msg =
 render : ViewConfig child msg -> Html msg
 render config =
     styled div
-        []
+        [ position absolute ]
         []
         (config.children
             |> List.map (config.childContent >> div [])
