b580b604d37e266d3c0fac018d3ab7337f5bc795
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-09 14:18:15 +0530

    elm-exp-store: popup menu simplification

--- experiments/elm-exp-store/src/index.js
+++ experiments/elm-exp-store/src/index.js
@@ -97,9 +97,9 @@ winFocusBlur$(document.hasFocus())
 
 window.addEventListener('focus', winFocusBlur$)
 window.addEventListener('blur', winFocusBlur$)
-window.addEventListener('focusout', winFocusBlur$)
+// window.addEventListener('focusout', winFocusBlur$)
 
-flyd.on(tapLog("window event"), winFocusBlur$)
+// flyd.on(tapLog("window event"), winFocusBlur$)
 
 const documentHasFocus$ = flyd.map(
   compose(() => document.hasFocus()),
