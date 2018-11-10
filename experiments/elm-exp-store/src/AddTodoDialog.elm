3efbc5d4ef9f65e26f68b6e0ca114034d16f398c
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-10 17:53:50 +0530

    elm-exp-store: layers

---
+++ experiments/elm-exp-store/src/AddTodoDialog.elm
@@ -0,0 +1,51 @@
+module AddTodoDialog exposing (Model, Msg(..), OutMsg(..), getBackdropDomId, rootDomId, update)
+
+import ContextStore exposing (ContextId)
+import DomX exposing (DomId)
+import Focus exposing (FocusResult)
+import Log
+import TodoStore exposing (TodoContent)
+import UpdateReturn exposing (..)
+
+
+type alias Model =
+    { content : TodoContent
+    , contextId : ContextId
+    }
+
+
+type OutMsg
+    = ActionOut TodoContent ContextId
+    | ClosedOut
+
+
+type Msg
+    = FocusResult FocusResult
+    | BackdropClicked DomId
+    | ContentChanged TodoContent
+    | ContextIdChanged ContextId
+
+
+rootDomId =
+    "add-todo-dialog"
+
+
+getBackdropDomId =
+    rootDomId (++) "-backdrop"
+
+
+update : Msg -> Model -> ( Model, Cmd Msg, Maybe OutMsg )
+update uniqueId message =
+    (case message of
+        FocusResult r ->
+            addCmd (Log.focusResult "ContextPopup.elm" r)
+                >> withNoOutMsg
+
+        BackdropClicked targetId ->
+            if targetId == getBackdropDomId uniqueId then
+                withOutMsg (\_ -> ClosedOut)
+
+            else
+                withNoOutMsg
+    )
+        << pure
