169936c6b0c2afcd10e77149c4b144ed44295c31
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-12 19:19:17 +0530

    elm-exp-store: layer

--- experiments/elm-exp-store/src/Layer.elm
+++ experiments/elm-exp-store/src/Layer.elm
@@ -1,10 +1,11 @@
 module Layer exposing (Layer(..), eqContextPopupFor)
 
+import BasicsX exposing (unwrapMaybe)
 import ContextDialog
 import ContextPopup
 import ContextStore exposing (Context, ContextId, ContextStore)
 import TodoDialog
-import UpdateReturn exposing (pure)
+import UpdateReturn exposing (..)
 
 
 type Layer
@@ -27,6 +28,7 @@ type Msg
     = TodoDialogMsg TodoDialog.Msg
     | ContextDialogMsg ContextDialog.Msg
     | ContextPopupMsg ContextPopup.Msg
+    | OpenTodoDialog TodoDialog.Msg TodoDialog.Model
 
 
 type OutMsg
@@ -40,14 +42,38 @@ withNoOutMsg ( m, c ) =
     ( m, c, NoOut )
 
 
+withOutMsg o ( m, c ) =
+    ( m, c, o )
+
+
 update : Msg -> Layer -> ( Layer, Cmd Msg, OutMsg )
-update message layer =
-    (case ( message, layer ) of
+update message layer_ =
+    (case ( message, layer_ ) of
         ( TodoDialogMsg msg, TodoDialog model ) ->
-            withNoOutMsg
+            updateTodoDialog msg model
+
+        ( OpenTodoDialog msg model, NoLayer ) ->
+            updateTodoDialog msg model
 
         _ ->
             withNoOutMsg
     )
     <|
-        pure layer
+        pure layer_
+
+
+updateTodoDialog msg todoDialog_ =
+    let
+        ( todoDialog, cmd, maybeOutMsg ) =
+            TodoDialog.update msg todoDialog_
+
+        handleOut =
+            unwrapMaybe
+                withNoOutMsg
+                (withOutMsg << TodoDialogOutMsg)
+                maybeOutMsg
+    in
+    pure
+        >> mapModel (\_ -> TodoDialog todoDialog)
+        >> addTaggedCmd TodoDialogMsg cmd
+        >> handleOut
