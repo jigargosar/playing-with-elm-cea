aee76c4ccdfc2ef16d8310afb0bce965189982be
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-12 18:58:55 +0530

    elm-exp-store: layer

--- experiments/elm-exp-store/src/Layer.elm
+++ experiments/elm-exp-store/src/Layer.elm
@@ -4,6 +4,7 @@ import ContextDialog
 import ContextPopup
 import ContextStore exposing (Context, ContextId, ContextStore)
 import TodoDialog
+import UpdateReturn exposing (pure)
 
 
 type Layer
@@ -20,3 +21,33 @@ eqContextPopupFor cid layer =
 
         _ ->
             False
+
+
+type Msg
+    = TodoDialogMsg TodoDialog.Msg
+    | ContextDialogMsg ContextDialog.Msg
+    | ContextPopupMsg ContextPopup.Msg
+
+
+type OutMsg
+    = TodoDialogOutMsg TodoDialog.OutMsg
+    | ContextDialogOutMsg ContextDialog.OutMsg
+    | ContextPopupOutMsg ContextPopup.OutMsg
+    | NoOut
+
+
+withNoOutMsg ( m, c ) =
+    ( m, c, NoOut )
+
+
+update : Msg -> Layer -> ( Layer, Cmd Msg, OutMsg )
+update message layer =
+    (case ( message, layer ) of
+        ( TodoDialogMsg msg, TodoDialog model ) ->
+            withNoOutMsg
+
+        _ ->
+            withNoOutMsg
+    )
+    <|
+        pure layer
