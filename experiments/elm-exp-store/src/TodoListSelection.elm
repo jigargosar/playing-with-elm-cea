c0204e312780e288caa4c01ad1a34eb3559701d7
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-14 18:37:52 +0530

    elm-exp-store: todo page

--- experiments/elm-exp-store/src/TodoListSelection.elm
+++ experiments/elm-exp-store/src/TodoListSelection.elm
@@ -1,5 +1,8 @@
 module TodoListSelection exposing (Model, SelectedIndex)
 
+import ContextStore exposing (ContextId)
+import TodoStore exposing (Todo, TodoStore)
+
 
 type alias SelectedIndex =
     Int
@@ -7,3 +10,14 @@ type alias SelectedIndex =
 
 type alias Model =
     SelectedIndex
+
+
+type alias Config =
+    { todoStore : TodoStore
+    , selectedContextId : ContextId
+    }
+
+
+getSelectedContextTodoList : Config -> List Todo
+getSelectedContextTodoList { todoStore, selectedContextId } =
+    todoStore |> TodoStore.listForContextId selectedContextId
